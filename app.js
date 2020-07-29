d3b9eda5f6ca1b3b6fe37645c6463f8498262138
Cody robbins <cody@MacBook-Pro.local>
2020-07-29 16:46:30 -0400

    header script added

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+const fs = require('fs');
+
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -117,7 +120,13 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
